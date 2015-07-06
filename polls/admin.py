f8d4c40e35fdd6af67546e4e2597900a19cbe7ed
author2006 <author2006@ukr.net>
2015-07-06 17:28:03 +0300

    Some changes in application model and configure administration interface for polls application.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from polls.models import Question, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 20
+    fieldsets = [
+        ('Content', {'fields': ['question_text'], 'classes': ['content-fieldset']}),
+        ('Date Setting', {'fields': ['pub_date'], 'classes': ['date-fieldset']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
