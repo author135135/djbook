949efd09acc54a5dfdf7c45174b4f88d4a85831a
author2006 <author2006@ukr.net>
2015-07-07 15:08:38 +0300

    Add new template file and some new view fuctions

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from polls.models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -13,14 +14,29 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, template_name='polls/detail.html', context={
-        'question': question
+        'question': question,
     })
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, template_name="polls/results.html", context={
+        'question': question,
+    })
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, template_name="polls/detail.html", context={
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+
+    choice.votes += 1
+    choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
